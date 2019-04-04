329ccc83f3155218924acc92790e5308728cecc5
Sam Singer <samuel.singer1@gmail.com>
2019-04-03 18:00:58 -0700

    Axios up and running

--- src/BookResultsContainer/BookResultsContainer.js
+++ src/BookResultsContainer/BookResultsContainer.js
@@ -5,18 +5,35 @@ class BookResultContainer extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            
+            search: null,
         }
     }
 
+    componentDidMount() {
+        
+    }
+
+    handleChange = (event) => {
+        event.preventDefault()
+        const { value } = event.target
+
+        this.setState({
+            search: value
+        })
+    }
+
     render () {
+        let submitIsActive = !this.state.search ? true : false;
+
         return (
             <div>
                 <input
                     className="searchInput" 
                     placeholder="Search Book"
+                    value={this.state.search}
+                    onChange={this.handleChange}
                 />
-                <button>SEARCH</button>
+                <button disabled={submitIsActive}>SEARCH</button>
             </div>
         )
     }
